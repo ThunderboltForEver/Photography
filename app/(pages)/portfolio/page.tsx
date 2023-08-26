9c474ecc4ddacc3bae3c5a078b6f16919de21d37
zana suleiman <zana.suleiman.44@gmail.com>
2023-08-26 13:29:38 +0300

    Added Changes

--- app/(pages)/portfolio/page.tsx
+++ app/(pages)/portfolio/page.tsx
@@ -27,8 +27,8 @@ export default function Portfolio() {
         </Link>
       </div>
       <div className=" flex flex-wrap justify-center md:grid md:grid-cols-2 gap-4 ">
-        {images.map((image) => (
-          <div className="overflow-hidden w-full sm:w-[70%] md:w-full">
+        {images.map((image,key) => (
+          <div className="overflow-hidden w-full sm:w-[70%] md:w-full" key={key}>
             <Image src={image} alt="" className="hover:scale-110 transition w-full" />
           </div>
         ))}
